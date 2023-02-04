3c30a1aeab60665d2a855b3caf02e6be495feaeb
sanjay <khardiyasanjay065@gmail.com>
2023-02-04 19:11:18 +0530

    cv link change

--- proj2_react/src/pages/Contact.js
+++ proj2_react/src/pages/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
         <span id='eidmark'>emailId: </span>
         <span id='eidvalue'>khardiyasanjay065@gmail.com</span>
         <br/><br/>
-        <a id='dlink' href='https://drive.google.com/uc?export=download&id=12N-JuhJ47rnNIZbnh-7qaHyJPIYA6Vz2' download>Download my CV</a>
+        <a id='dlink' href='https://drive.google.com/uc?export=download&id=1aFlNxpq2LfBwsMpk9-7Dfl5G5Nwf7ROs' download>Download my CV</a>
       </div>
     );
   };
